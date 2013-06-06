d7fdb0db14cbf04a663ce162881e76ab82b430b2
Brett Mack <brett.mack@foxienet.com>
2013-06-06 23:07:28 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SvnFilter::Application.config.secret_token = '2ffd2c6456e55dd15e39ec6ca281b2ee3eedfcf15d9a191c9c6578a36415392607f2108d1bd94aee3b70654d5e83ad5873c5bcb529bdb9cae97c735d354a7a1a'
