e1da7568c8032a518245c9a38e7a8fb1bba35419
Brett Mack <brett.mack@foxienet.com>
2013-06-17 01:44:00 +0100

    tidy up of specs to better fit in with the documentation format

--- spec/models/repo_spec.rb
+++ spec/models/repo_spec.rb
@@ -1,7 +1,29 @@
 require 'spec_helper'
 
-describe Repo do
-	context 'with valid model' do
+describe "An external", Repo do
+		
+	it 'should be properly initialized' do
+		expect(create(:repo)).to be_a(Repo)
+	end
+
+	it "must have unique name" do
+		myrepo = create(:repo)
+		expect(build(:repo, name: myrepo.name)).to have(1).errors_on(:name)
+	end
+
+	it "only allows an alphanumeric name" do
+		expect(build(:repo, name: "Invalid Name")).to have(1).errors_on(:name)
+	end
+
+	context 'when empty' do
+		let(:empty_repo) { Repo.build }
+
+		it { expect(:empty_repo).to have_at_least(1).errors_on(:name) }
+		it { expect(:empty_repo).to have_at_least(1).errors_on(:protocol) }
+		it { expect(:empty_repo).to have_at_least(1).errors_on(:url) }
+	end
+
+	context 'with valid data' do
 		let(:repo) { create(:repo) }
 		it { expect(repo).to be_valid }
 
@@ -10,23 +32,6 @@ describe Repo do
 				expect(build(:repo, protocol: p)).to be_valid
 			end
 		end
-	end
-
-	context 'with invalid model' do
-		let(:empty_repo) { Repo.build }
-		it { expect(:empty_repo).to have_at_least(1).errors_on(:name) }
-		it { expect(:empty_repo).to have_at_least(1).errors_on(:protocol) }
-		it { expect(:empty_repo).to have_at_least(1).errors_on(:url) }
-
-		it "must have unique name" do
-			myrepo = create(:repo)
-			expect(build(:repo, name: myrepo.name)).to have(1).errors_on(:name)
-		end
-
-		it "only allows an alphanumeric name" do
-			expect(build(:repo, name: "Invalid Name")).to have(1).errors_on(:name)
-		end
-
 	end
 
 end
