11eaa7d08c213cc16df67360d56803b9bb27e15f
Brett Mack <brett.mack@foxienet.com>
2013-06-26 14:41:13 +0100

    Added Svnsync worker to process the creation/syncing of local repos in the background. Required sidekiq/testing in spec_helper to help when testing background tasks.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,9 @@ Spork.prefork do
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
+  require 'sidekiq/testing'
+
+  DATA_DIR = File.expand_path("../data",__FILE__)
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
